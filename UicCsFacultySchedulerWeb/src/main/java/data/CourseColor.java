be9c540d99339ddf5cf29179b91415e675d50a6c
Bryan Spahr <bspahr314@gmail.com>
2015-04-21 13:40:35 -0500

    better colors

--- UicCsFacultySchedulerWeb/src/main/java/data/CourseColor.java
+++ UicCsFacultySchedulerWeb/src/main/java/data/CourseColor.java
@@ -2,14 +2,14 @@ package data;
 
 public interface CourseColor {
 	//HTML Hex values for colors
-	public static final String PRE200_BG = "#ffeeee";
+	public static final String PRE200_BG = "#E598F5";
 	public static final String PRE200_TX = "#000000";
-	public static final String PRE300_BG = "#ff0";
+	public static final String PRE300_BG = "#FAFA48";
 	public static final String PRE300_TX = "#000000";
-	public static final String PRE400_BG = "#ff0000";
+	public static final String PRE400_BG = "#F74A4A";
 	public static final String PRE400_TX = "#ffffff";
-	public static final String PRE500_BG = "#11e";
+	public static final String PRE500_BG = "#6363FF";
 	public static final String PRE500_TX = "#ffffff";
-	public static final String DEFAULT_COL_BG = "#3a87ad";
+	public static final String DEFAULT_COL_BG = "#63EB7E";
 	public static final String DEFAULT_COL_TX = "#ffffff";
 }
